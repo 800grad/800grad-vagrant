0f46552d43ce616bbabaedf969fd9b3a3f393b3a
Benjamin Wunn <b.wunn@webweit.de>
2019-01-31 00:17:39 +0100

    init

--- Vagrantfile
+++ Vagrantfile
@@ -1,38 +1,33 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
-
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-    config.vm.box = "v0rtex/xenial64"
+    config.vm.box = "ubuntu/bionic64"
 
-    # Mount shared folder using NFS
-    config.vm.synced_folder ".", "/vagrant",
+    config.vm.synced_folder ".", "/grav",
         id: "core",
         :nfs => true,
         :mount_options => ['nolock,vers=3,udp,noatime']
 
-    # Do some network configuration
     config.vm.network "private_network", ip: "192.168.11.11"
 
-    # Assign a quarter of host memory and all available CPU's to VM
-    # Depending on host OS this has to be done differently.
     config.vm.provider :virtualbox do |vb|
         host = RbConfig::CONFIG['host_os']
 
         if host =~ /darwin/
-            cpus = `sysctl -n hw.ncpu`.to_i
+            cpus = `sysctl -n hw.ncpu`.to_i / 2
             mem = `sysctl -n hw.memsize`.to_i / 1024 / 1024 / 4
 
         elsif host =~ /linux/
             cpus = `nproc`.to_i
             mem = `grep 'MemTotal' /proc/meminfo | sed -e 's/MemTotal://' -e 's/ kB//'`.to_i / 1024 / 4
 
-        # Windows...
+        elsif host =~ /mswin32/
+            cpus = `echo %NUMBER_OF_PROCESSORS%`.to_i / 2
+            mem = `wmic computersystem get TotalPhysicalMemory`.to_i / 1024 / 1024 / 4
         else
             cpus = 4
-            mem = 2048
+            mem = 4096
         end
 
         vb.customize ["modifyvm", :id, "--memory", mem]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,72 +1,79 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# mixins
 Update () {
     echo "-- Update packages --"
-    sudo apt-get update
-    sudo apt-get upgrade
+    sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt-get autoremove -y
 }
-Update
 
-echo "-- Prepare configuration for MySQL --"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+#the real install process
+echo "-- update system --"
+Update
+sudo apt-get dist-upgrade -y
 
 echo "-- Install tools and helpers --"
-sudo apt-get install -y --force-yes python-software-properties vim htop curl git npm build-essential libssl-dev
-
-echo "-- Install PPA's --"
-sudo add-apt-repository ppa:ondrej/php
-sudo add-apt-repository ppa:chris-lea/redis-server
+sudo apt-get install -y nano gcc g++ make wget htop curl git git-core build-essential libssl-dev php-cli php-zip iotop unzip
 Update
 
 echo "-- Install NodeJS --"
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_11.x | bash -
+sudo apt-get install -y nodejs yarn
+Update
 
-echo "-- Install packages --"
-sudo apt-get install -y --force-yes apache2 mysql-server-5.7 git-core nodejs rabbitmq-server redis-server
-sudo apt-get install -y --force-yes php7.1-common php7.1-dev php7.1-json php7.1-opcache php7.1-cli libapache2-mod-php7.1
-sudo apt-get install -y --force-yes php7.1 php7.1-mysql php7.1-fpm php7.1-curl php7.1-gd php7.1-mcrypt php7.1-mbstring
-sudo apt-get install -y --force-yes php7.1-bcmath php7.1-zip
+echo "-- Install NodeJS Modules--"
+sudo npm update -g
+npm install node-sass gulp gulp-sass gulp-prettier gulp-cssnano --save-dev
+
+# Install Apache and modules required to run Grav
+echo "-- Install Apache --"
+sudo add-apt-repository ppa:ondrej/apache2 -y
+sudo apt-get install -y apache2
+sudo a2enmod rewrite
+Update
+
+echo "-- Install PHP --"
+sudo add-apt-repository ppa:ondrej/php -y
+Update
+
+sudo apt-get install -y libmcrypt-dev php7.3 php7.3-bcmath php7.3-cli php7.3-common php7.3-curl php7.3-dev php7.3-fpm php7.3-gd php7.3-intl php7.3-json php7.3-mbstring php7.3-opcache php7.3-xml php7.3-zip libapache2-mod-php7.3 php-apcu
 Update
 
 echo "-- Configure PHP &Apache --"
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.1/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/apache2/php.ini
-sudo a2enmod rewrite
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/apache2/php.ini
+sudo a2enmod rewrite proxy_fcgi setenvif
+sudo a2enconf php7.3-fpm
+sudo systemctl restart apache2
+Update
+
 
 echo "-- Creating virtual hosts --"
 sudo ln -fs /vagrant/public/ /var/www/app
 cat << EOF | sudo tee -a /etc/apache2/sites-available/default.conf
 <Directory "/var/www/">
     AllowOverride All
+    Require all granted
 </Directory>
 
 <VirtualHost *:80>
-    DocumentRoot /var/www/app
-    ServerName app.local
-</VirtualHost>
-
-<VirtualHost *:80>
-    DocumentRoot /var/www/phpmyadmin
-    ServerName phpmyadmin.local
+    DocumentRoot /var/www/grav
+    ServerName 800grad.local
 </VirtualHost>
 EOF
 sudo a2ensite default.conf
 
 echo "-- Restart Apache --"
-sudo /etc/init.d/apache2 restart
+sudo systemctl restart apache2
 
 echo "-- Install Composer --"
 curl -s https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 sudo chmod +x /usr/local/bin/composer
 
-echo "-- Install phpMyAdmin --"
-wget -k https://files.phpmyadmin.net/phpMyAdmin/4.8.0.1/phpMyAdmin-4.8.0.1-english.tar.gz
-sudo tar -xzvf phpMyAdmin-4.8.0.1-english.tar.gz -C /var/www/
-sudo rm phpMyAdmin-4.8.0.1-english.tar.gz
-sudo mv /var/www/phpMyAdmin-4.8.0.1-english/ /var/www/phpmyadmin
+#install grav
+composer create-project getgrav/grav /var/www/
 
-echo "-- Setup databases --"
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql -uroot -proot -e "CREATE DATABASE my_database";
+#remove default grav userfolder and clone correct one
+cd /var/www/
+rm -rf user
+git clone --recurse-submodules https://github.com/800grad/user.git
